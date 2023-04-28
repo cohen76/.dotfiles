2aded7a02a33e2b70c188369519c0a54d7e089e0
Jacob Cohen <cohen76@proton.me>
2023-04-27 19:57:12 -0700

    added mint install script

---
+++ setup_mint.bash
@@ -0,0 +1,32 @@
+config_system() {
+  xdg-settings set default-web-browser 'brave-browser.desktop'
+}
+
+config_gnome() {
+  # sets tab & shift + tab to navigate between terminal tabs
+  gsettings set org.gnome.Terminal.Legacy.Keybindings:/org/gnome/terminal/legacy/keybindings/ next-tab '<Primary>Tab'
+  gsettings set org.gnome.Terminal.Legacy.Keybindings:/org/gnome/terminal/legacy/keybindings/ prev-tab '<Primary><Shift>Tab'
+}
+
+install_brave() {
+  sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+  echo 'deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main' | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+  sudo apt update -y && sudo apt install -y brave-browser
+}
+
+install_codium() {
+  wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
+    | gpg --dearmor \
+    | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
+  echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https://download.vscodium.com/debs vscodium main' \
+    | sudo tee /etc/apt/sources.list.d/vscodium.list
+  sudo apt update -y && sudo apt install -y codium
+}
+
+install_git() {
+  sudo apt install -y git
+}
+
+install_vlc() {
+  sudo apt install -y vlc
+}
